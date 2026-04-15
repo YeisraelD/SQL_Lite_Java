828839dc48aa0393175a44a792b697fdd462c29f
YeisraelD <Yeisraeld@gmail.com>
2026-04-15 22:06:20 +0300

    Commit 8: Execution Engine - Create & Insert

---
+++ src/neondb/ExecutionEngine.java
@@ -0,0 +1,69 @@
+package neondb;
+
+import neondb.parser.*;
+import neondb.storage.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class ExecutionEngine {
+    private Map<String, TableManager> tables = new HashMap<>();
+
+    public void execute(Statement stmt) throws Exception {
+        if (stmt == null) return;
+        
+        if (stmt instanceof CreateTableStatement) {
+            executeCreateTable((CreateTableStatement) stmt);
+        } else if (stmt instanceof InsertStatement) {
+            executeInsert((InsertStatement) stmt);
+        } else if (stmt instanceof SelectStatement) {
+            executeSelect((SelectStatement) stmt);
+        } else {
+            throw new Exception("Unsupported statement type.");
+        }
+    }
+
+    private void executeCreateTable(CreateTableStatement stmt) throws Exception {
+        if (tables.containsKey(stmt.tableName)) {
+            throw new Exception("Table already exists: " + stmt.tableName);
+        }
+        
+        // Ensure data directory exists
+        java.io.File dataDir = new java.io.File("data");
+        if (!dataDir.exists()) {
+            dataDir.mkdir();
+        }
+
+        Table table = new Table(stmt.tableName, stmt.columns);
+        TableManager tm = new TableManager(table, "data/" + stmt.tableName + ".ndb");
+        tables.put(stmt.tableName, tm);
+        System.out.println("Table " + stmt.tableName + " created.");
+    }
+
+    private void executeInsert(InsertStatement stmt) throws Exception {
+        TableManager tm = tables.get(stmt.tableName);
+        if (tm == null) {
+            throw new Exception("Table not found: " + stmt.tableName);
+        }
+        Table table = tm.getTable();
+        List<Column> columns = table.getColumns();
+
+        if (stmt.values.size() != columns.size()) {
+            throw new Exception("Column count doesn't match value count.");
+        }
+
+        Map<String, Object> data = new HashMap<>();
+        for (int i = 0; i < columns.size(); i++) {
+            data.put(columns.get(i).getName(), stmt.values.get(i));
+        }
+
+        Row row = new Row(data);
+        tm.insert(row);
+        System.out.println("1 row inserted.");
+    }
+
+    public void executeSelect(SelectStatement stmt) throws Exception {
+        // Will be implemented in Commit 9
+    }
+}
