bd8d0a3246cc443c04824845c85332de1b74b61b
YeisraelD <Yeisraeld@gmail.com>
2026-04-16 20:52:45 +0300

    added test querie

---
+++ test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE test_users (id INT, name STRING)
+INSERT INTO test_users VALUES (1, "mrA")
+INSERT INTO test_users VALUES (2, "mrB")
+INSERT INTO test_users VALUES (3, "mrC")
+INSERT INTO test_users VALUES (4, "mrD")
+INSERT INTO test_users VALUES (5, "mrE")
+SELECT * FROM test_users
+.exit
